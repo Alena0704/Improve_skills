bb133075738ddd77aa3905221edfa8c093b31443
Alena0704 <lena.ribackina@yandex.ru>
2021-10-14 18:37:55 +0300

    update Context2

--- Context2.py
+++ Context2.py
@@ -2,6 +2,7 @@
 Describe problem in
 https://contest.yandex.ru/contest/28738/problems/A/
 '''
+
 def desicion_determine_max():
     maximum = 0
     num_maximal = 0
@@ -49,6 +50,7 @@ class DecisionB:
 Describe problem in
 https://contest.yandex.ru/contest/28738/problems/C/
 '''
+
 def desicion_polindrom():
     str_ = str(input())
     max=0
