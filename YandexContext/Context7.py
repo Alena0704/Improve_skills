c7191d74c53815c130aff5e75071f782a891d184
Alena0704 <lena.ribackina@yandex.ru>
2021-10-30 19:35:44 +0300

    decision context7 A

--- YandexContext/Context7.py
+++ YandexContext/Context7.py
@@ -3,49 +3,24 @@ Describe problem in
 https://contest.yandex.ru/contest/29396/problems/A/
 '''
 def DecisionA():
-        k = int(input())
-        dict_ = {}
-        for _ in range(k):
-            L,R = map(int, input().split())
-            if L==R:
-                continue
-            if len(dict_)==0:
-                dict_[(L,R)] = R-L
-                continue
-            del_s, del_e = -1, -1
-            for elem in dict_:
-                s, e = elem[0], elem[1]
-                if L >=s and L < e:
-                    if R > e:
-                        dict_[(s,R)] = R-s
-                        del_s = s
-                        del_e = e
-                        break
-                    else:
-                        continue
-                elif R >= s and R < e:
-                    if L < s:
-                        dict_[(L,e)] = e-L
-                        del_s = s
-                        del_e = e
-                        break
-                    else:
-                        continue
-                dict_[(L,R)] = R-L
-            if del_s * del_e > 0:
-                del dict_[(del_s, del_e)]
+    k = int(input())
+    lst = []
+    for _ in range(k):
+        l, r = map(int, input().split())
+        lst.append((l,1))
+        lst.append((r,-1))
         
-        length = sum(dict_.values())
-        return length
+    lst.sort()
+    depth = 0; #intersecting segments
+    res = 0 #result length
+    p = lst[0][1]
+    res+=lst[1][0]-lst[0][0]
+    if k>1:
+        for i in range(1, k*2-1):
+            if p + lst[i][1] != 0:
+                res += lst[i+1][0] - lst[i][0]
+            p+=lst[i][1]
+    print(res)
 
-k = int(input())
-dict_={}
-for i in range(k):
-	h_s, h_f, g = map(int, input().split())
-	if (h_s, h_f) not in dict_:
-    		dict_[(h_s, h_f)] = g+1
-	else:
-		    dict_[(h_s, h_f)] += g+1
-keys = list(dict_.values())
-keys.sort()
-print(keys[0])
+DecisionA()
+
