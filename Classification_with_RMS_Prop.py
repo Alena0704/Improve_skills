5d160fd44a4762283d7662845dcb42e54c8d5cd8
Alena0704 <lena.ribackina@yandex.ru>
2021-11-01 18:50:29 +0300

    add classificator model with rms_prop

---
+++ Classification_with_RMS_Prop.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython import display
+from Classificarion_SGD import Model_SGD
+
+class Model_RMSPROP(Model_SGD):
+    def __init__(self, X, y, n_iter, eta=0.05, batch_size=4, alpha = 0.9, eps = 1e-8):
+        super().__init__(X, y, n_iter, eta, batch_size)
+        self.g2 = np.zeros_like(self.w) # we start with None so that you can update this value correctly on the first iteration
+        self.G = np.zeros_like(self.w)
+        self.eps = eps
+        self.alpha = 0.9
+
+    def fit(self):
+        loss = np.zeros(self.n_iter)
+        plt.figure(figsize=(12, 5))
+
+        for i in range(self.n_iter):
+            ind = np.random.choice(self.X.shape[0], self.batch_size)
+            loss[i] = self.compute_loss()
+            if i % 10 == 0:
+                self.visualize(self.X[ind, :], self.y[ind], self.w, loss)
+            
+            self.G = self.alpha * self.G + (1-self.alpha)*(self.compute_grad()**2)
+            self.w = self.w - self.eta*self.compute_grad()/(np.sqrt(self.G + self.eps))
+
+        self.visualize(self.X, self.y, self.w, loss)
