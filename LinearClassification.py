3fca3c10002a8feec54ccd9a48bb68ef804450ec
Alena0704 <lena.ribackina@yandex.ru>
2021-11-02 21:41:06 +0300

    add generator batches in the fit

--- LinearClassification.py
+++ LinearClassification.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Classificarion_SGD import Model_SGD
+from Classification_Momentum import Nesterov
 from Classification_Adam import Model_Momentum, Model_Adam, Model_RMSPROP
+from Classification_Momentum import Nesterov
 
 def show_picture(X, y):
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired, s=20)
@@ -13,14 +15,17 @@ with open('train.npy', 'rb') as fin:
 with open('target.npy', 'rb') as fin:
     y = np.load(fin)
 
-model_SGD = Model_SGD(X,y,100)
+model_SGD = Model_SGD(X,y,10,8)
 model_SGD.fit()
 
-model_momentum = Model_Momentum(X,y,100)
+model_momentum = Model_Momentum(X,y,10,8)
 model_momentum.fit()
 
-model_Rms = Model_RMSPROP(X,y,100)
+model_momentum = Nesterov(X,y,10,8)
+model_momentum.fit()
+
+model_Rms = Model_RMSPROP(X,y,10,8)
 model_Rms.fit()
 
-model_adam = Model_Adam(X,y,100)
+model_adam = Model_Adam(X,y,10,8)
 model_adam.fit()
